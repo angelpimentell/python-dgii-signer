0f0bad57a21f01948f76b2e33206d1339ff5ed8b
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-02-14 09:15:33 -0400

    Change signer unit test

--- tests/signer_unit_test.py
+++ tests/signer_unit_test.py
@@ -1,14 +1,14 @@
 from signers import DgiiSigner
-import pathlib
 import os
 from helpers import clean_xml
 
 
 def test_sign_invoice():
     # Arrange
-    signer = DgiiSigner(f"{pathlib.Path().resolve()}{os.sep}files{os.sep}certificate.p12", "admin")
-    xml_content = open(f"{pathlib.Path().resolve()}{os.sep}files{os.sep}invoice.xml").read()
-    expected_xml_content = open(f"{pathlib.Path().resolve()}{os.sep}files{os.sep}invoice_signed.xml", encoding="utf-8").read()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    signer = DgiiSigner(f"{current_dir}{os.sep}files{os.sep}certificate.p12", "admin")
+    xml_content = open(f"{current_dir}{os.sep}files{os.sep}invoice.xml").read()
+    expected_xml_content = open(f"{current_dir}{os.sep}files{os.sep}invoice_signed.xml", encoding="utf-8").read()
 
     # Act
     signed_xml_content = signer.sign(xml_content)
